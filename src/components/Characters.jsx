7925179338617ad221350e0cbd6b5976c0ca6b77
Aldo Notebook <aldoaliscioni18@gmail.com>
2022-07-11 16:56:22 -0300

    changed characters page

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -17,7 +17,7 @@ const Characters = () => {
   };
 
   const handleNotFound = () => {
-    return `El personaje no fue encontrado `
+    return <p className={style.dontFound}>El personaje no fue encontrado</p>
   }
 
   useEffect(() => {
@@ -28,21 +28,18 @@ const Characters = () => {
     }
   }, [filterCharacters.succeess]);
 
-  useEffect(() => {
-    ref.current.value = '';
-
-  }, []);
-
   return (
     <div className={style.container}>
       <h2 className='title'>Characters List</h2>
-      <input ref={ref} className={style.input} type="text" onChange={handleOnChange} placeholder='Search Character' />
-
+      <div className={style.inputContain}>
+        <svg className={style.glass} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" /></svg>
+        <input ref={ref} className={style.input} type="text" onChange={handleOnChange} placeholder='Search Character' />
+      </div>
       {filterCharacters.error ? handleNotFound() :
         <div className={characters ? style.charactersCointaner : style.containerLoading}>
           {filterCharacters.succeess.length > 0 ? renderCharacters(filterCharacters.succeess) :
             (characters && ref.current?.value === "") ? renderCharacters(characters) :
-            !characters ? <Loading /> : ''}
+              !characters ? <Loading /> : ''}
         </div>}
     </div>
   )
